142f9ce3fd3d45bfda0e4aab723d3fa50ac23c01
Thomas Gebert <thomas@gebert.sexy>
2017-10-20 00:34:40 -0400

    Add zookeeper helper functions

---
+++ src/shove/zookeeper.clj
@@ -0,0 +1,24 @@
+(ns shove.zookeeper
+  (:import 
+  (org.apache.zookeeper CreateMode)
+  (org.apache.zookeeper WatchedEvent)
+  (org.apache.zookeeper Watcher)
+  ;(org.apache.zookeeper.Watcher.Event KeeperState)
+  ;(org.apache.zookeeper ZooDefs.Ids)
+  (org.apache.zookeeper ZooKeeper))
+  (:gen-class))
+
+(defn createZookeeper [^String url] 
+  (let
+      [
+       watcher (reify Watcher (^void process [this ^WatchedEvent e] nil))
+       zk (new ZooKeeper url 3000 watcher)
+      ] zk))
+
+(defn getValues [zk path]
+  (.getChildren zk path false))
+
+(defn getData [zk path]
+  (new String (.getData zk path false nil))
+  )
+
