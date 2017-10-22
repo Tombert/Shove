cae7f96b78c36eaf91a6fde48d8d9815e2e8f0b3
Thomas Gebert <thomas@gebert.sexy>
2017-10-22 02:14:57 -0400

    Break out the handlers to a map.
    This should make it fairly to extract into separate file

--- src/shove/core.clj
+++ src/shove/core.clj
@@ -195,10 +195,10 @@
   
   )
 
-(defn handler-fn [{:keys [event] :as all-data}]
-     (case event
-       :done-add-zookeeper
-          (let [
+(def handlemap {
+  :done-add-zookeeper 
+      (fn [event all-data state]
+                        (let [
                 {{{nbf :text} :new-zookeeper-field} :fn-fx/includes} all-data
                 {zookeepers :zookeepers} @state
 
@@ -210,23 +210,11 @@
                 zookeeperStr (str/join "\n" finalZookeepers)
                ] 
             (swap! state assoc :add-zookeeper false :zookeepers finalZookeepers)
-            (spit zookeeperfile zookeeperStr))
-       :add-zookeeper (swap! state assoc :add-zookeeper true)
-       :delete-zookeeper
-           (do (println "Howdy" (str all-data))
-           (let [
-                 
-                    {{{nbf :value} :zookeeper-field} :fn-fx/includes} all-data
-                    {zookeepers :zookeepers} @state
-                    finalZookeepers (filter (fn [x] (println x) (not= x nbf)) zookeepers)
-                    zookeeperStr (str/join "\n" finalZookeepers)
-                 ]
-                (println "Thing" (str nbf))
-                (swap! state assoc :zookeepers finalZookeepers)
-                (spit zookeeperfile zookeeperStr)             
-             ))
-
-       :zookeepers-selected (do (println "\n\n\n\n\n\n\n yo dawg\n\n\n\n\n\n" (str all-data))
+            (spit zookeeperfile zookeeperStr)))
+    :add-zookeeper (fn [event all-data state] (swap! state assoc :add-zookeeper true))
+ 
+    :zookeepers-selected 
+        (fn [event all-data state] (do (println "\n\n\n\n\n\n\n yo dawg\n\n\n\n\n\n" (str all-data))
                  (let [
                        {includes :fn-fx/includes} all-data
                        {{bf :value} :zookeeper-field} includes
@@ -240,9 +228,23 @@
                        topics (mapv identity (zookeeper/getValues zk "/brokers/topics"))
                        ] (println "\n\nbrokers: " (str topics)) 
                     (swap! state assoc :brokers brokers :topics topics)
-                   )
-                 )
-       :import-csv
+                   )))
+    :delete-zookeeper
+        (fn [event all-data state]
+           (do (println "Howdy" (str all-data))
+           (let [
+                 
+                    {{{nbf :value} :zookeeper-field} :fn-fx/includes} all-data
+                    {zookeepers :zookeepers} @state
+                    finalZookeepers (filter (fn [x] (println x) (not= x nbf)) zookeepers)
+                    zookeeperStr (str/join "\n" finalZookeepers)
+                 ]
+                (println "Thing" (str nbf))
+                (swap! state assoc :zookeepers finalZookeepers)
+                (spit zookeeperfile zookeeperStr))))
+
+     :import-csv
+        (fn [event all-data state]
            (let [
                  {includes :fn-fx/includes}  all-data
                  {{bf :value } :zookeeper-field {tf :text} :topic-field} includes
@@ -254,23 +256,96 @@
                  jsonstuff (map #(json/write-str %1) mdata )
 
                  ]
-                 (doall (map #(sendMessage bf tf (str (java.util.UUID/randomUUID)) %1) jsonstuff))
+                 (doall (map #(sendMessage bf tf (str (java.util.UUID/randomUUID)) %1) jsonstuff))))
 
-             
-             
-                 
-             ) 
-           
-       :auth (let [
+       :auth (fn [event all-data state] (let [
                    ;; Extracts out the fields from the big object that JavaFX gives us
                    ;; TODO: This is uglier than it should be, might break up to multiple lines
                    {{{kf :text} :key-field {bf :value} :zookeeper-field {tf :text} :topic-field {cf :text} :content-field} :fn-fx/includes} all-data
 
              ] 
                (sendMessage bf tf kf cf) 
+               (println "Unknown UI event" event all-data)))
+ })
+
+(defn handler-fn [{:keys [event] :as all-data}]
+     ((get handlemap event) event all-data state)
+     ;(case event
+     ;  :done-add-zookeeper
+     ;     (let [
+     ;           {{{nbf :text} :new-zookeeper-field} :fn-fx/includes} all-data
+     ;           {zookeepers :zookeepers} @state
+
+     ;           sortedZookeepers (-> zookeepers
+     ;                            (conj nbf) 
+     ;                            distinct
+     ;                            sort)
+     ;           finalZookeepers (filter (fn [x] (not= x "")) sortedZookeepers)
+     ;           zookeeperStr (str/join "\n" finalZookeepers)
+     ;          ] 
+     ;       (swap! state assoc :add-zookeeper false :zookeepers finalZookeepers)
+     ;       (spit zookeeperfile zookeeperStr))
+     ;  :add-zookeeper (swap! state assoc :add-zookeeper true)
+     ;  :delete-zookeeper
+     ;      (do (println "Howdy" (str all-data))
+     ;      (let [
+                 
+     ;               {{{nbf :value} :zookeeper-field} :fn-fx/includes} all-data
+     ;               {zookeepers :zookeepers} @state
+     ;               finalZookeepers (filter (fn [x] (println x) (not= x nbf)) zookeepers)
+     ;               zookeeperStr (str/join "\n" finalZookeepers)
+     ;            ]
+     ;           (println "Thing" (str nbf))
+     ;           (swap! state assoc :zookeepers finalZookeepers)
+     ;           (spit zookeeperfile zookeeperStr)             
+     ;        ))
+
+     ;  :zookeepers-selected (do (println "\n\n\n\n\n\n\n yo dawg\n\n\n\n\n\n" (str all-data))
+     ;            (let [
+     ;                  {includes :fn-fx/includes} all-data
+     ;                  {{bf :value} :zookeeper-field} includes
+     ;                  zk (zookeeper/createZookeeper bf)
+     ;                  brokerids (zookeeper/getValues zk "/brokers/ids")
+     ;                  jsonbrokers (map (fn [x] (zookeeper/getData zk (str "/brokers/ids/" x) )) brokerids)
+     ;                  lbrokers (mapcat (fn [x] (let [{ep "endpoints"} (json/read-str x)] ep)) jsonbrokers)
+     ;                  brokers (mapv (fn [x] (str/replace x #"PLAINTEXT://" "")) lbrokers)
+
+     ;                  ;brokers (doall (fn [x] (zookeeper/getValues (str "/brokers/ids/" x))) brokerids)
+     ;                  topics (mapv identity (zookeeper/getValues zk "/brokers/topics"))
+     ;                  ] (println "\n\nbrokers: " (str topics)) 
+     ;               (swap! state assoc :brokers brokers :topics topics)
+     ;              )
+     ;            )
+     ;  :import-csv
+     ;      (let [
+     ;            {includes :fn-fx/includes}  all-data
+     ;            {{bf :value } :zookeeper-field {tf :text} :topic-field} includes
+     ;            window (.getWindow (.getScene (:target (:fn-fx/event includes))))
+     ;            dialog (doto (FileChooser.) (.setTitle "Import CSV"))
+     ;            file (util/run-and-wait (.showOpenDialog dialog window))
+     ;            data (with-open [reader (io/reader file)] (doall (csv/read-csv reader)))
+     ;            mdata (csv-data->maps data)
+     ;            jsonstuff (map #(json/write-str %1) mdata )
+
+     ;            ]
+     ;            (doall (map #(sendMessage bf tf (str (java.util.UUID/randomUUID)) %1) jsonstuff))
+
+             
+             
+                 
+     ;        ) 
+           
+     ;  :auth (let [
+     ;              ;; Extracts out the fields from the big object that JavaFX gives us
+     ;              ;; TODO: This is uglier than it should be, might break up to multiple lines
+     ;              {{{kf :text} :key-field {bf :value} :zookeeper-field {tf :text} :topic-field {cf :text} :content-field} :fn-fx/includes} all-data
+
+     ;        ] 
+     ;          (sendMessage bf tf kf cf) 
                
                
-                       (println "Unknown UI event" event all-data))))
+     ;                  (println "Unknown UI event" event all-data)))
+  )
 
 (defn -main []
   (spit zookeeperfile "" :append true)
